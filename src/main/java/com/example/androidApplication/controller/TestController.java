9f8d7804e1bc2338a87bf120df59f978acf3a833
01086055223@hanmail.net <01086055223@hanmail.net>
2021-11-10 18:04:05 +0900

    [OTH]Delete TestCode ToAWS Upload

--- src/main/java/com/example/androidApplication/controller/TestController.java
+++ src/main/java/com/example/androidApplication/controller/TestController.java
@@ -1,8 +1,11 @@
 package com.example.androidApplication.controller;
 
 import com.example.androidApplication.domain.entity.Member;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class TestController {
